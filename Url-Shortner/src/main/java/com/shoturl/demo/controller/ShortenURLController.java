fe024411dfaca3e4c998bcf818c277ab2cc1b522
abc <abc@sang>
2023-02-21 19:44:49 +0530

    first commit

---
+++ Url-Shortner/src/main/java/com/shoturl/demo/controller/ShortenURLController.java
@@ -0,0 +1,12 @@
+package com.shoturl.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShortenURLController {
+	@GetMapping("/")
+	public String loadIndex() {
+		return "index";
+	}
+}
---
+++ Url-Shortner/src/test/java/com/shoturl/demo/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoturl.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
